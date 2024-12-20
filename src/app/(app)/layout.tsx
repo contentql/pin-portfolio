4514aef3f4d43f0fc400fb1ca5dca429524955a7
charan <charanm927@gmail.com>
2024-12-20 10:14:02 +0530

    chore: build fix

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -99,9 +99,9 @@ function hexToHsl(hex: string): string {
   hex = hex?.replace(/^#/, '')
 
   // Parse the hex color
-  const r = parseInt(hex.substring(0, 2), 16) / 255
-  const g = parseInt(hex.substring(2, 4), 16) / 255
-  const b = parseInt(hex.substring(4, 6), 16) / 255
+  const r = parseInt(hex?.substring(0, 2), 16) / 255
+  const g = parseInt(hex?.substring(2, 4), 16) / 255
+  const b = parseInt(hex?.substring(4, 6), 16) / 255
 
   // Find min and max values
   const max = Math.max(r, g, b)
